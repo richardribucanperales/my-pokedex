c1bb141fecc04986106f52d990d70fbe87a507f9
gawigwen <19rodnce@waipahuh.k12.hi.us>
2017-12-06 14:26:45 -1000

    done

--- js/app.js
+++ js/app.js
@@ -4,13 +4,13 @@
 // Pokemon 1
 // Find the div with the id of "name1" and replace the text n/a with "Tentacool"
 // Place your code below
-
+document.getElementById("name1").innerHTML = "Tentacool";
 
 
 // Pokemon 2
 // Find the div with the id of "type2" and replace the text n/a with "water"
 // Place your code below
-
+document.getElementById("type2").innerHTML = "water";
 
 
 
@@ -18,7 +18,7 @@
 // Find the div with the id of "pokeNumber3" and replace the text n/a with 116:
 // Place your code below
 
-
+document.getElementById("pokeNumber3").innerHTML = 116;
 
 
 
@@ -26,7 +26,7 @@
 // Find the div with the id of "bio4" and replace the text n/a with your personal description of this Pokemon.
 // Place your code below
 
-
+document.getElementById("bio4").innerHTML = "i honestly do not know, its a water type and it probably shoots water";
 
 
 
@@ -34,14 +34,14 @@
 // Find the div with the id of "bio5" and replace the the font color to "red";
 // Place your code below
 
-
+document.getElementById("bio5").style.color = "red";
 
 
 
 // Pokemon 6
 // Find the div with the id of "name6" and replace the font size to "60px";
 // Place your code below
-
+document.getElementById("name6").style.fontSize = "60px";
 
 
 
@@ -49,5 +49,4 @@
 // Pokemon 7
 // Find the div with the id of "container" and change the background color to "green"
 // Place your code below
-
-
+document.getElementById("container").style.backgroundColor = "green";
